a2fd6dae707a18d9055bd826e568a11646aafeda
Yofita Indah <yofitaindah@gmail.com>
2025-01-15 13:50:42 +0700

    first commit

---
+++ app/map/Map.jsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React, { useRef, useEffect } from "react";
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+import "../map/map.css";
+import adminData from "../map/bgn.geojson";
+
+const MapComponent = () => {
+  const mapRef = useRef(null);
+  const map = useRef(null);
+  const lng = 106.862; // Longitude Jakarta
+  const lat = -6.2222; // Latitude Jakarta
+  const zoom = 15; // Zoom level
+  const API_KEY = "LwdMyEmnYMoMgeVadnRo"; // MapTiler API Key
+
+  useEffect(() => {
+    if (map.current) return; // Prevent map from initializing more than once
+
+    map.current = new maplibregl.Map({
+      container: mapRef.current,
+      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
+      center: [lng, lat],
+      zoom: zoom,
+    });
+
+    // Add navigation controls
+    map.current.addControl(new maplibregl.NavigationControl(), "top-right");
+
+    map.current.on("load", () => {
+      // Add GeoJSON source
+      map.current.addSource("admin", {
+        type: "geojson",
+        data: adminData,
+      });
+
+      // Add 3D extrusion layer
+      map.current.addLayer({
+        id: "admin-extrusion-layer",
+        type: "fill-extrusion",
+        source: "admin",
+        paint: {
+          "fill-extrusion-color": "blue",
+          "fill-extrusion-height": ["get", "height2"], // Height attribute for extrusion
+          "fill-extrusion-base": 0, // Base of extrusion
+        },
+      });
+
+      // Uncomment the following code if you want to add a stroke layer
+      /*
+      map.current.addLayer({
+        id: "admin-stroke-layer",
+        type: "line",
+        source: "admin",
+        paint: {
+          "line-color": "red", // Stroke color
+          "line-width": 1.5, // Stroke width
+        },
+      });
+      */
+    });
+  }, [API_KEY, lng, lat, zoom]);
+
+  return (
+    <div
+      ref={mapRef}
+      className="map"
+      style={{ height: "100vh", width: "100%" }}
+    />
+  );
+};
+
+export default MapComponent;
---
+++ app/map/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MapComponent from "./map";
+
+const page = () => {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <MapComponent />
+    </div>
+  );
+};
+
+export default page;
